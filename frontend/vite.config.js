9ee313340a8d5e2e34fe12051b3c2b746913000e
Akhilesh26 <akhileshpimple3@gmail.com>
2024-09-06 10:07:13 +0530

    small change

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,7 +1,11 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  server: {
+    proxy: {
+      '/api': 'http://localhost:4000', // Adjust the port to match your Node.js backend
+    },
+  },
+});
