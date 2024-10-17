2374ccc48855b98e408d2bbd0794360eabaf19a9
Akhilesh26 <akhileshpimple3@gmail.com>
2024-10-17 18:08:16 +0530

    add new proxy route for server for handlng api calls

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,11 +1,11 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
   server: {
     proxy: {
-      '/api': 'http://localhost:4000', // Adjust the port to match your Node.js backend
+      '/api': 'https://glacial-lake-outburst-floods-system.onrender.com', // Adjust the port to match your Node.js backend
     },
   },
 });
