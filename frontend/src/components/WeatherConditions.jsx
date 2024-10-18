c2cdddb2332bc8560139f08302fb419d0e77e085
Akhilesh26 <akhileshpimple3@gmail.com>
2024-10-18 19:35:35 +0530

    add div for desgin change for data visualization

--- frontend/src/components/WeatherConditions.jsx
+++ frontend/src/components/WeatherConditions.jsx
@@ -1,7 +1,7 @@
-import  { useState } from 'react';
+import { useState } from 'react';
 import axios from 'axios';
 import { Link } from 'react-router-dom';
-import { Line} from 'react-chartjs-2';
+import { Line } from 'react-chartjs-2';
 import { Chart as ChartJS, CategoryScale, LinearScale, PointElement, LineElement, Title, Tooltip, Legend } from 'chart.js';
 
 ChartJS.register(CategoryScale, LinearScale, PointElement, LineElement, Title, Tooltip, Legend);
@@ -22,7 +22,7 @@ const WeatherConditions = () => {
             setError("Failed to fetch weather data. Please try again later.");
         }
     };
-  
+
     const chartData = {
         labels: ['Temperature', 'Humidity'],
         datasets: [
@@ -42,7 +42,7 @@ const WeatherConditions = () => {
 
     return (
         <div>
-            <input className='border-2 border-black py-2 px-4 m-4 bg-slate-400 rounded-md' type="text" value={place} onChange={(e)=>setPlace(e.target.value)}/>
+            <input className='border-2 border-black py-2 px-4 m-4 bg-slate-400 rounded-md' type="text" value={place} onChange={(e) => setPlace(e.target.value)} />
             <button className='border py-2 px-4 border-black rounded-lg bg-slate-900 text-slate-200 mx-2' onClick={handleWeatherData}>Submit</button>
             <Link to="/airQuality"><button className='border py-2 px-4 border-black rounded-lg bg-slate-900 text-slate-200 mx-2'>Air Quality</button></Link>
             <Link to="/temp-humidity"><button className='border py-2 px-4 border-black rounded-lg bg-slate-900 text-slate-200 mx-2' >Temp and humidity</button></Link>
@@ -50,14 +50,16 @@ const WeatherConditions = () => {
             {weatherData && (
                 <div>
                     <h3>Weather Conditions:</h3>
-                    
+
                     <p>Place Name: {weatherData.location.name}</p>
                     <p>Region: {weatherData.location.region}</p>
                     <p>Country: {weatherData.location.country}</p>
                     <p>Temperature: {weatherData.current.temp_c}°C</p>
                     <p>Condition: {weatherData.current.condition.text}</p>
                     <p>Wind Speed in MPH: {weatherData.current.wind_mph}</p>
-                    <Line data={chartData} />
+                    <div className='w-[500px] h-[500px]'>
+                        <Line data={chartData} />
+                    </div>
                 </div>
             )}
         </div>
