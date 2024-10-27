80e14c42c83113a580fce5479a7d89676731e785
Akhilesh26 <akhileshpimple3@gmail.com>
2024-10-27 22:56:04 +0530

    change test to deployment proxy

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -5,8 +5,7 @@ export default defineConfig({
   plugins: [react()],
   server: {
     proxy: {
-      '/api': 'http://localhost:3000', // Adjust the port to match your Node.js backend
-      // '/api': 'https://glacial-lake-outburst-floods-system.onrender.com', // Adjust the port to match your Node.js backend
+      '/api': 'https://glacial-lake-outburst-floods-system.onrender.com', // Adjust the port to match your Node.js backend
 
     },
   },
